e7b64406df7b05ad28c2c8aa2bdd07202a420b7a
codingMustache <jorcar1986@gmail.com>
2022-12-19 02:26:29 -0600

    Made a lil toy

---
+++ index.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const prompt = require('prompt-sync')()
+
+const areYouANepoBby = name =>{
+  const sendName = name.split(' ').join('_')
+  axios.get(`https://en.wikipedia.org/wiki/${sendName}`)
+    .then(({data}) => {
+      const $ = cheerio.load(data)
+      const test = $('th.infobox-label').map((_, each) => {
+				const $each = $(each);
+        if($each.text() === 'Family' ||
+          $each.text() === 'Relatives' ||
+          $each.text() === 'Parents'){
+          const row = $each.siblings('.infobox-data')
+          return row.children().text()
+        }
+		  }).toArray()
+    if(!!test.length){
+      return console.log('Yeah, they\'re a nepo baby!')
+    }else {
+      return console.log('I didm\'t find anything...')
+   }
+  })
+}
+const input = prompt('Enter a potential nepo baby:')
+areYouANepoBby(input)
